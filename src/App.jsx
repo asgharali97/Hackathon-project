51116d0660272dbe7d4c09c987ef2710047d0f47
asgharali97 <asgharalifs48@gmail.com>
2024-10-07 22:58:27 -0700

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
+import { useEffect } from "react"
+import Lenis from 'lenis'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Outlet } from "react-router-dom"
 
 function App() {
+  useEffect(()=>{
+    const lenis = new Lenis()
+
+    lenis.on('scroll', (e) => {
+      console.log(e)
+    })
+    
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+    
+    requestAnimationFrame(raf)
+     
+  },[])
   
 
   return (
     <>
+    <div className=" h-screen">
+      <Header/>
+      <main>
+        <Outlet/>
+      </main>
+     <Footer/>
      
+    </div>
+    
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>this footter
+    </div>
+  )
+}
+
+export default Footer
